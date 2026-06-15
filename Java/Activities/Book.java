51e7dacd03cbff948b16924b940a6820fa47f642
ldas123 <daskabir135@gmail.com>
2026-06-15 12:34:09 +0530

    Add files via upload

---
+++ Java/Activities/Book.java
@@ -0,0 +1,13 @@
+package activities;
+
+public abstract class Book {
+	
+	String title;
+	
+	public abstract void setTitle(String title);
+	
+	public String getTitle() {
+		return title;
+	}
+
+}
---
+++ Java/Activities/MyBook.java
@@ -0,0 +1,11 @@
+package activities;
+
+public class MyBook extends Book{
+
+	// Define abstract method
+	public void setTitle(String str) {
+		this.title = str;
+		
+	}
+
+}
