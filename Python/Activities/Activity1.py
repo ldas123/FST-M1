5e22aa46432c89c57d500b14f4671321217a4a3f
ldas123 <daskabir135@gmail.com>
2026-06-18 10:50:33 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+year = str((2026-age)+100)
+print(name+" will turn 100 years old in year "+year)
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+number = int(input("Enter any number: "))
+mod = number%2
+if mod==0:
+    print("Number is even number")
+else:
+    print("Number is odd number")
