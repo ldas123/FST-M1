d680e97de6822cb9f48691b33d91535704a2d0a3
ldas123 <daskabir135@gmail.com>
2026-06-22 12:21:43 +0530

    Add files via upload

---
+++ Python/Activities/Activity18.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# Read the CSV file and store it into a DataFrame
+dataframe = pd.read_csv("activity.csv")
+
+# Print the full dat
+print("Data: ")
+print(dataframe)
+
+	
+# Print the values in the Usernames column only
+print("===============")
+print("Usernames:")
+print(dataframe["Usernames"])
+
+	
+# Print the username and password of the second row
+	
+print("===============")
+print("Username: ", dataframe["Usernames"][1], " | ", "Password: ",dataframe["Passwords"][1])
+
+#Sort the Usernames column in ascending order
+print("====================================")
+print("Data sorted in ascending Usernames:")
+print(dataframe.sort_values('Usernames'))
+
+	
+#Sort the Passwords column in descending order
+print("====================================")
+print("Data sorted in descending Passwords:")
+print(dataframe.sort_values('Passwords', ascending=False))
