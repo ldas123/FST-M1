87eb89a11897e943f5a78171106f8ccf1c01087b
ldas123 <daskabir135@gmail.com>
2026-06-23 10:29:34 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,35 @@
+package javaSeleniumexamples;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Instantiate ChromeDriver
+		WebDriver driver = new ChromeDriver();
+		
+		//Navigating to the page
+		driver.get("https://training-support.net");
+		
+		//Getting the title of the page
+		System.out.println("Title of page is: "+driver.getTitle());
+		
+		Thread.sleep(Duration.ofSeconds(3));
+		
+		//Click the about us button
+		driver.findElement(By.xpath("//*[text()='About Us']")).click();
+		
+		//Print the title of the new page
+		System.out.println("Title of page is: "+driver.getTitle());
+		
+		//Closing the page
+		driver.close();;
+
+	}
+
+}
---
+++ Selenium/Activities/Activity2.java
@@ -0,0 +1,37 @@
+package javaSeleniumexamples;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		//Instantiate ChromeDriver
+				WebDriver driver = new ChromeDriver();
+				
+				//Navigating to the page
+				driver.get("https://training-support.net/webelements/login-form/");
+				
+				//Get the title of the page and print it to the console
+				System.out.println("Title of page is: "+driver.getTitle());
+				
+				Thread.sleep(Duration.ofSeconds(3));
+				
+				//Find the username field using any locator and enter "admin" into it
+				driver.findElement(By.id("username")).sendKeys("admin");
+				
+				//Find the password field using any locator and enter "password" into it
+				driver.findElement(By.id("password")).sendKeys("password");
+				
+				//Find the "Log in" button using any locator and click it.
+				driver.findElement(By.xpath("//button[text()='Submit']")).click();
+				
+				//Close the browser
+				driver.close();
+
+	}
+
+}
